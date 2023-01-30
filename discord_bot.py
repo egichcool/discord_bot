9148d03ea608c071ad6c00d97bf03b2a96b42e7c
Egorka <egich.cool@gmai.com>
2023-01-30 18:24:54 +0300

    Done weather embeded message

--- discord_bot.py
+++ discord_bot.py
@@ -3,16 +3,19 @@ import asyncio
 from API_KEYS import BOT_TOKEN, TEST_CHANEL_ID, CHAT_CHANEL_ID, API_KEY, EGICHCOOL_ID
 import openai
 from openmeteo_py import Options,OWmanager
+import json
 
 
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        openai.api_key = API_KEY             
+        openai.api_key = API_KEY        
+        with open('data/weather_code.json', 'r') as json_file:
+            self.weather_codes = json.load(json_file)
           
 
     async def setup_hook(self) -> None:
-        # create the background task and run it in the background
+        # Создать таск на фоне
         self.bg_task = self.loop.create_task(self.my_background_task())
 
     async def on_ready(self):
@@ -23,24 +26,39 @@ class MyClient(discord.Client):
         options = Options(*self.coords, current_weather=True)
         mgr = OWmanager(options)
         meteo = mgr.get_data()
-        print(meteo)
+        #print(meteo)
         return meteo['current_weather']
+
+    async def get_embed_weather(self):
+        weather_data = await self.get_weather()    
+
+        embed = discord.Embed(title='Прогноз погоды', 
+                        description=f"""За бортом {weather_data['temperature']} 
+                        {self.weather_codes[str(weather_data['weathercode'])]['name']}""",
+                        color=int(self.weather_codes[str(weather_data['weathercode'])]['color'], 16))
+        
+        if weather_data['time'][11:13] in ('07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19'):
+            embed.set_thumbnail(url=f"{self.weather_codes[str(weather_data['weathercode'])]['image_day']}")
+        else:
+            embed.set_thumbnail(url=f"{self.weather_codes[str(weather_data['weathercode'])]['image_night']}")
+        
+        return embed
 
     async def my_background_task(self):
         await self.wait_until_ready()
         counter = 0
-        channel = self.get_channel(TEST_CHANEL_ID)  # channel ID goes here
-        weather_data = await self.get_weather()          
-        embed = discord.Embed(title='Прогноз погоды', 
-                              description=f"За бортом {weather_data['temperature']}, {self.weather_codes[weather_data['weathercode']]}",
-                              color=0xFFFF00)     
-        embed.set_thumbnail(url='https://lh5.googleusercontent.com/JuQZdHpH1JyBm3SBVyY7ePWyNwFOEn_JipegmgFALmKat441MI0qCbzFQdpiKvdcQiA=w2400')        
-        weather_message = await channel.send('Приветик', embed=embed)
+        channel = self.get_channel(TEST_CHANEL_ID)  # ID канала для отправки погоды   
+
+        embed = await self.get_embed_weather()    # Тут получать данные месаги
+        
+        weather_message = await channel.send(embed=embed)
+
         while not self.is_closed():
-            await asyncio.sleep(5)  # task runs every 60 seconds
+            await asyncio.sleep(10)  # Каждые N секунд перезапускать таск
             counter += 1
             #await channel.send(counter)
-            await weather_message.edit(embed=embed, content='')
+            embed = await self.get_embed_weather()
+            await weather_message.edit(embed=embed)
 
 
             '''
